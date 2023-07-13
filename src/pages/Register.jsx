c48c2543dc720a642195a58e5e30a900ed58a7c0
Anjali Arya <anjalisinha498@gmail.com>
2023-07-13 23:58:48 +0530

    firebase-setup

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,13 +1,60 @@
 import React from "react";
-// import Add from "../img/add.png";
+import {  createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
 
-import {  Link } from "react-router-dom";
+// import Add from "../img/add.png";
+// import {  Link } from "react-router-dom";
 
 const Register = () => {
 
-  const handleSubmit = () => {
-    //
-  };
+  const [err, setErr] = useState(false)
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const displayName=e.target[0].value;
+    const email=e.target[1].value;
+    const password=e.target[2].value;
+    const file=e.target[3].files[0];
+    
+    try{
+
+      const res = await createUserWithEmailAndPassword(auth, email, password);
+    //Create a unique image name
+    const date = new Date().getTime();
+    const storageRef = ref(storage, `${displayName + date}`);
+
+    await uploadBytesResumable(storageRef, file).then(() => {
+      getDownloadURL(storageRef).then(async (downloadURL) => {
+        try {
+          //Update profile
+          await updateProfile(res.user, {
+            displayName,
+            photoURL: downloadURL,
+          });
+          //create user on firestore
+          await setDoc(doc(db, "users", res.user.uid), {
+            uid: res.user.uid,
+            displayName,
+            email,
+            photoURL: downloadURL,
+          });
+
+          //create empty user chats on firestore
+          await setDoc(doc(db, "userChats", res.user.uid), {});
+          navigate("/");
+        } catch (err) {
+          console.log(err);
+          setErr(true);
+          setLoading(false);
+        }
+      });
+    });
+  } catch (err) {
+    setErr(true);
+    setLoading(false);
+  }
+};
 
   return (
     <div className="formContainer">
@@ -15,16 +62,16 @@ const Register = () => {
         <span className="logo">Chatroom</span>
         <span className="title">Register</span>
         <form onSubmit={handleSubmit}>
-          <input required type="text" placeholder="display name" />
-          <input required type="email" placeholder="email" />
-          <input required type="password" placeholder="password" />
-          <input required style={{ display: "none" }} type="file" id="file" />
+          <input  type="text" placeholder="display name"  />
+          <input  type="email" placeholder="email" />
+          <input  type="password" placeholder="password" />
+          <input  style={{ display: "none" }} type="file" id="file" />
           <label htmlFor="file">
-            <h1>+</h1>
             <img src="./img/add.png" alt="" />
             <span>Add an avatar</span>
           </label>
           <button>Sign up</button>
+          {err && <span>Something went wrong!</span>}
         </form>
         <p>
           {/* You do have an account? <Link to="">Login</Link> */}
