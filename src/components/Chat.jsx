cf434600d7e5d56ac9d173b616756533928b84d9
Anjali Arya <anjalisinha498@gmail.com>
2023-07-28 23:58:03 +0530

    input & chatcontext

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -4,13 +4,17 @@ import React from "react";
 // import More from "../img/more.png";
 import Messages from "./Messages";
 import Input from "./Input";
+import { ChatContext } from "../context/ChatContext";
+
 
 const Chat = () => {
+
+  const {data} = useContext(ChatContext);
 
   return (
     <div className="chat">
       <div className="chatInfo">
-        <span>Anjali arya</span>
+        <span>{data.user?.displayName}</span>
         <div className="chatIcons">
           <img src="./img/cam.png" alt="" />
           <img src="./img/add.png" alt="" />
